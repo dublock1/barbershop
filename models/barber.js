8fa38e1fdfb23e95be71f88f4000cc1a7cd6989c
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-12 13:35:40 -0400

    added schema and exported functions

--- models/barber.js
+++ models/barber.js
@@ -4,7 +4,9 @@ const mongoose = require('./connection.js')
 const BarberSchema = new mongoose.Schema({
  name: String,
  email: String,
- experience: Number
+ experience: Number,
+ bio: String,
+ image: String
 })
 
 
@@ -12,10 +14,30 @@ const BarberCollection = mongoose.model('Barber', BarberSchema)
 
 
 function getAllBarbers() {
-  return 
+  return BarberCollection.find()
+}
+
+function getBarber(barberId) {
+    return BarberCollection.findById(barberId)
+}
+
+function addNewBarber(barberObject) {
+    return BarberCollection.create(barberObject)
+}
+
+function updateBarber(barberId, updateBarber) {
+    return BarberCollection.findByIdAndUpdate(barberId, updateBarber)
+}
+
+function deleteBarber(barberId) {
+    return BarberCollection.findByIdAndDelete(barberId)
 }
 
 
 module.exports = {
-  getHelloWorldString
+  getAllBarbers,
+  getBarber,
+  addNewBarber,
+  updateBarber,
+  deleteBarber
 }

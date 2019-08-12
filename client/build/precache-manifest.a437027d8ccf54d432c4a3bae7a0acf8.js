f4c721b38ad8071984cbeb3181687a6c05705e94
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-12 12:46:28 -0400

    init commit client

---
+++ client/build/precache-manifest.a437027d8ccf54d432c4a3bae7a0acf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b256a83eaa7dcb9d14b9725051de05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d68904837c8e3674bcf",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "4d68904837c8e3674bcf",
+    "url": "/static/js/main.769c3406.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

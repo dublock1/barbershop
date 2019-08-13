baa3d0e48a1fbb08903d448a9c5d7bc3772e7639
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-13 09:35:04 -0400

    added router s

--- controllers/barber.js
+++ controllers/barber.js
@@ -1,43 +1,45 @@
-/* Step 1 import express
- *
- */
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
-const templateApi = require('../models/template.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
-const templateRouter = express.Router()
+const barberApi = require('../models/barber.js')
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
-templateRouter.get('/', (req, res) => {
-  res.json(templateApi.getHelloWorldString())
+const barberRouter = express.Router()
+
+ 
+barberRouter.get('/', (req, res) => {
+    barberApi.getAllBarbers()
+    .then((barbers) => {
+        res.json(barbers)
+    })
+})
+
+barberRouter.get('/:barberId', (req, res) => {
+    barberApi.getBarber(req.params.barberId)
+    .then((barber) => {
+        res.json(barber)
+    })
+})
+
+barberRouter.post('/', (req, res) => {
+    barberApi.addNewBarber(req.body)
+    .then((barber) => {
+        res.json(barber)
+    })
+})
+
+barberRouter.put('/:barberId', (req, res) => {
+    barberApi.updateBarber(req.params.barberId, req.body)
+    .then((updateBarber) => {
+        res.json(updateBarber)
+    })
+})
+
+barberRouter.delete('/:barberId', (req, res) => {
+    barberApi.deleteBarber(req.params.barberId)
+    .then((deleteBarber) => {
+        res.json(deleteBarber)
+    })
 })
 
 /* Step 6
@@ -46,5 +48,5 @@ templateRouter.get('/', (req, res) => {
  *
  */
 module.exports = {
-  templateRouter
+  barberRouter
 }

58a062bd787b7c4c64187934a887cf2d050a0b9e
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-13 13:40:40 -0400

    added route and component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HelloWorld from './components/HelloWorld.js'
 import './App.css';
+import BarbershopList from './components/BarbershopList.js';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={HelloWorld}/>
+          <Route exact path="/" component={BarbershopList}/>
         </Switch>
       </Router>
     </div>

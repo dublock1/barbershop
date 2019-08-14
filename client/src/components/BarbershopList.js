546f389642cce2b4dd1553452b13623573c818ba
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-14 10:45:07 -0400

    added h2 tags

--- client/src/components/BarbershopList.js
+++ client/src/components/BarbershopList.js
@@ -1,43 +1,50 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import {Button} from 'react-bootstrap'
 
 export default class BarbershopList extends Component {
-    state = {
-        newBarbershop: {
-            name: '',
-            address: '',
-            currentBarber: '',
-            image: ''
-        },
-        barbershops: []
-    }
+  state = {
+    newBarbershop: {
+      name: "",
+      address: "",
+      currentBarber: "",
+      image: ""
+    },
+    barbershops: []
+  };
 
-    componentDidMount() {
-        this.getAllBarbershops();
-    }
+  componentDidMount() {
+    this.getAllBarbershops();
+  }
 
-    getAllBarbershops = () => {
-        axios.get('/api/memes').then(res => {
-            this.setState({barbershops: res.data})
-        })
-    }    
-    
-    render() {
+  getAllBarbershops = () => {
+    axios.get("/api/barbershops").then(res => {
+      this.setState({ barbershops: res.data });
+    });
+  };
 
-        const barbershopList = this.state.barbershops.map(barbershop => {
-            return(
-                <div>
-
-                <Link key={barbershop._id} to ={`/barbershops/${barbershop._id}`}>
-                    <h2>name={barbershop.name}</h2>
-                    <h2>address={barbershop.address}</h2>
-                    <h2>currentBarber={barbershop.currentBarber}</h2>
-                    <img src ={barbershop.image} />
-                </Link>
-                </div>
-            )
-        })
-        return barbershopList
-    }
+  render() {
+    const barbershopList = this.state.barbershops.map(barbershop => {
+      return (
+        <div>
+          <Link key={barbershop._id} to={`/barbershops/${barbershop._id}`}>
+            <h2>{barbershop.name}</h2>
+            <h2>{barbershop.address}</h2>
+            <h2>Current Barbers: {barbershop.currentBarber}</h2>
+            <img src={barbershop.image} />
+          </Link>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <h1>All Good</h1>
+        <Link to={'/barbershops/new'}>
+        <Button>Add Barbershop</Button>
+        </Link>
+        {barbershopList}
+      </div>
+    );
+  }
 }

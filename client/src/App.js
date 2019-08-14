9699673873c833c7d4674d202ecb7bced33b2b7c
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-14 10:43:22 -0400

    fixed a typo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import BarbershopList from './components/BarbershopList.js';
+import BarbershopForm from './components/BarbershopForm';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={BarbershopList}/>
+          <Route path='/barbershops/new' component={BarbershopForm}/>
         </Switch>
       </Router>
     </div>

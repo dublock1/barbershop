54c22fe4ae640f9c0cb5c623d464b334f5b9ae0c
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-16 12:29:30 -0400

    added barber Routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import BarbershopList from './components/BarbershopList.js';
 import BarbershopForm from './components/BarbershopForm';
+import Barbershop from './components/Barbershop';
+import BarberList from './components/BarberList';
+import BarberForm from './components/BarberForm';
+import Barber from './components/Barber'
+import EditBarberForm from './components/EditBarberForm';
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
         <Switch>
           <Route exact path="/" component={BarbershopList}/>
           <Route path='/barbershops/new' component={BarbershopForm}/>
+          <Route path='/barbershops/:barbershopId' component={Barbershop} />
+          <Route exact path='/barbers' component={BarberList}/>
+          <Route path='/barbers/new' component={BarberForm}/>
+          <Route path='/barbers/:barberId' component={Barber}/>
+          <Route exact path='/barbers/:barberId/edit' component={EditBarberForm}/>
         </Switch>
       </Router>
     </div>

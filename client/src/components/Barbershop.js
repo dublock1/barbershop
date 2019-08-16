83a3655a91baae65946708c8b5126001cf2f89de
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-16 12:30:27 -0400

    added edit button

--- client/src/components/Barbershop.js
+++ client/src/components/Barbershop.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {Redirect} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
 
 export default class Barbershop extends Component {
+    state = {
+        redirectToHome: false,
+        newBarbershop: {
+            name: '',
+            address: '',
+            currentBarbers: '',
+            image: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`/api/barbershops/${this.props.match.params.barbershopId}`).then(res => {
+            this.setState({newBarbershop: res.data})
+        })
+    }
+
+    handleDelete = () => {
+        axios.delete(`/api/barbershops/${this.state.newBarbershop._id}`).then(() => {
+            this.setState({redirectToHome: true})
+        })
+    }
+
     render() {
+        if(this.state.redirectToHome) {
+            return <Redirect to='/' />
+        }
         return (
             <div>
-                
+                <p>{this.state.newBarbershop.name}</p>
+                <p>{this.state.newBarbershop.address}</p>
+                <li>{this.state.newBarbershop.currentBarbers}</li>
+                <img src={this.state.newBarbershop.image} />
+                <Button onClick={this.handleDelete}>Delete Shop</Button>
             </div>
         )
     }

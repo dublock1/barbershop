b3c1c6e9c10992a083ae2ae2d6127ee65de68014
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-18 12:58:20 -0400

    get all style request

--- client/src/components/StyleList.js
+++ client/src/components/StyleList.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import {Button} from 'react-bootstrap'
+
 
 export default class StyleList extends Component {
     state ={
-        name: '',
-        image: ''
+        newStyle: {
+            name: '',
+            image: ''
+        },
+        style: []
     }
+
+    componentDidMount() {
+        this.getAllStyles()
+    }
+
+    getAllStyles = () => {
+        axios.get('/api/styles').then(res => {
+            this.setState({style: res.data})
+        })
+    }
+
     render() {
-        return (
-            <div>
-                
-            </div>
+        const styleList = this.state.style.map(style => {
+            return(
+                <div>
+                    <Link to={`/styles/${style._id}`}>
+                    <h2>Type of Cut: {style.name}</h2>
+                    <img src={style.image}/>
+                    </Link>
+                </div>
+            )
+        })
+
+            return (
+                <div>
+                    <Link to={'/styles/new'}>
+                    <Button>Add Style</Button>
+                    </Link>
+                    {styleList}                
+                </div>
         )
     }
 }

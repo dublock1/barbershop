e4195fece291e0bb69982760d55a329a9f511e11
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-18 12:57:50 -0400

    post request

--- client/src/components/StyleForm.js
+++ client/src/components/StyleForm.js
@@ -1,11 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 export default class StyleForm extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+  state = {
+    newStyle: {
+      name: "",
+      image: ""
+    },
+    redirectToHome: false
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    axios.post("/api/styles", this.state.newStyle).then(() => {
+      this.setState({
+        redirectToHome: true
+      });
+    });
+  };
+
+  handleInputChange = e => {
+    const copiedStyle = { ...this.state.newStyle };
+    copiedStyle[e.target.name] = e.target.value;
+
+    this.setState({ newStyle: copiedStyle });
+  };
+
+  render() {
+    if (this.state.redirectToHome) {
+      return <Redirect to="/styles" />;
     }
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-style-name">Type of Cut</label>
+          <input
+            type="text"
+            name="name"
+            id="new-style-name"
+            onChange={this.handleInputChange}
+            value={this.state.newStyle.name}
+          />
+
+          <label htmlFor="new-style-image">Style Image</label>
+          <input
+            type="text"
+            name="image"
+            id="new-style-image"
+            onChange={this.handleInputChange}
+            value={this.state.newStyle.image}
+          />
+
+          <input type="submit" value="Add Style" />
+        </form>
+      </div>
+    );
+  }
 }

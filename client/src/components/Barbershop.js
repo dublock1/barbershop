b5c0a99887b3846c4a941bcb4ca569339e6bcb7a
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-19 16:35:56 -0400

    map and added ul

--- client/src/components/Barbershop.js
+++ client/src/components/Barbershop.js
@@ -1,43 +1,68 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import {Redirect} from 'react-router-dom'
-import {Button} from 'react-bootstrap'
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class Barbershop extends Component {
-    state = {
-        redirectToHome: false,
-        newBarbershop: {
-            name: '',
-            address: '',
-            currentBarbers: '',
-            image: ''
-        }
-    }
+  state = {
+    redirectToHome: false,
+    newBarbershop: {
+      name: "",
+      address: "",
+      currentBarbers: [],
+      image: ""
+    },
+    barbers: []
+  };
 
-    componentDidMount() {
-        axios.get(`/api/barbershops/${this.props.match.params.barbershopId}`).then(res => {
-            this.setState({newBarbershop: res.data})
-        })
-    }
+  componentDidMount() {
+    axios
+      .get(`/api/barbershops/${this.props.match.params.barbershopId}`)
+      .then(res => {
+        this.setState({
+          newBarbershop: res.data.barbershop,
+          barbers: res.data.barbers
+        });
+      });
+  }
 
-    handleDelete = () => {
-        axios.delete(`/api/barbershops/${this.state.newBarbershop._id}`).then(() => {
-            this.setState({redirectToHome: true})
-        })
-    }
+  handleDelete = () => {
+    axios
+      .delete(`/api/barbershops/${this.state.newBarbershop._id}`)
+      .then(() => {
+        this.setState({ redirectToHome: true });
+      });
+  };
 
-    render() {
-        if(this.state.redirectToHome) {
-            return <Redirect to='/' />
-        }
-        return (
-            <div>
-                <p>{this.state.newBarbershop.name}</p>
-                <p>{this.state.newBarbershop.address}</p>
-                <li>{this.state.newBarbershop.currentBarbers}</li>
-                <img src={this.state.newBarbershop.image} />
-                <Button onClick={this.handleDelete}>Delete Shop</Button>
-            </div>
-        )
+  render() {
+    if (this.state.redirectToHome) {
+      return <Redirect to="/" />;
     }
+    return (
+      <div>
+        <p>{this.state.newBarbershop.name}</p>
+        <p>{this.state.newBarbershop.address}</p>
+        <li>{this.state.newBarbershop.currentBarbers}</li>
+        <img src={this.state.newBarbershop.image} />
+        <ul>
+          {this.state.barbers.map(barber => {
+            return (
+              <div>
+                <li>{barber.name}</li>
+              </div>
+            );
+          })}
+        </ul>
+        <Button onClick={this.handleDelete}>Delete Shop</Button>
+        <Link
+          to={`/barbershops/${
+            this.props.match.params.barbershopId
+          }/barbers/new`}
+        >
+          <Button>Add Barber</Button>
+        </Link>
+      </div>
+    );
+  }
 }

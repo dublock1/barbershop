39e7d09734a9434046acef3485ebe173fb3f81bc
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-20 20:14:40 -0400

    form boostrap

--- client/src/components/EditBarberForm.js
+++ client/src/components/EditBarberForm.js
@@ -1,97 +1,117 @@
-import React, { Component } from 'react'
-import {Redirect} from 'react-router-dom'
-import axios from 'axios'
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+import { Form, Col, Button } from "react-bootstrap";
+
 export default class EditBarberForm extends Component {
-    state = {
-        newBarber: {
-          name: "",
-          email: "",
-          experience: "",
-          bio: "",
-          image: ""
-        },
-        redirectToHome: false
-      };
+  state = {
+    newBarber: {
+      name: "",
+      email: "",
+      experience: "",
+      bio: "",
+      image: ""
+    },
+    redirectToHome: false
+  };
 
-      componentDidMount(){
-        axios.get(`/api/barbers/${this.props.match.params.barberId}`).then(res => {
-            this.setState({newBarber: res.data})
-        })
+  componentDidMount() {
+    axios.get(`/api/barbers/${this.props.match.params.barberId}`).then(res => {
+      this.setState({ newBarber: res.data.barber });
+    });
+  }
+
+  handleInputChange = e => {
+    const copiedBarber = { ...this.state.newBarber };
+    copiedBarber[e.target.name] = e.target.value;
+
+    this.setState({ newBarber: copiedBarber });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios
+      .put(`/api/barbers/${this.state.newBarber._id}`, this.state.newBarber)
+      .then(res => {
+        this.setState({
+          newBarber: res.data,
+          redirectToHome: true
+        });
+      });
+  };
+  render() {
+    if (this.state.redirectToHome) {
+      return <Redirect to={`/barbers/${this.props.match.params.barberId}`} />;
     }
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridName">
+              <Form.Label htmlFor="new-barber-name">Barber Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                id="new-barber-name"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.name}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label htmlFor="new-barber-email">Barber email</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                id="new-barber-email"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.email}
+              />
+            </Form.Group>
+          </Form.Row>
 
-      handleInputChange = e => {
-        const copiedBarber = { ...this.state.newBarber };
-        copiedBarber[e.target.name] = e.target.value;
-    
-        this.setState({ newBarber: copiedBarber });
-      };
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridImage">
+              <Form.Label htmlFor="new-barber-image">Barber Image</Form.Label>
+              <Form.Control
+                type="text"
+                name="image"
+                id="new-barber-image"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.image}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridExp">
+              <Form.Label htmlFor="new-barber-experience">
+                Barber experience
+              </Form.Label>
+              <Form.Control
+                type="text"
+                name="experience"
+                id="new-barber-experience"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.experience}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label htmlFor="new-barber-bio">Barber Bio</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="3"
+              type="text"
+              name="bio"
+              id="new-barber-bio"
+              onChange={this.handleInputChange}
+              value={this.state.newBarber.bio}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Update Barber
+          </Button>
 
-      handleSubmit = event => {
-        event.preventDefault();
-    
-        axios
-          .put(`/api/barbers/${this.state.newBarber._id}`, this.state.newBarber)
-          .then(res => {
-            this.setState({
-              newBarber: res.data,
-              redirectToHome: true
-            });
-          });
-      };
-    render() {
-        if (this.state.redirectToHome) {
-            return <Redirect to="/barbers" />;
-          }
-          return (
-          <div>
-              <form onSubmit={this.handleSubmit}>
-                      <label htmlFor='new-barber-name'>Barber Name</label>
-                          <input
-                          type='text'
-                          name='name'
-                          id='new-barber-name'
-                          onChange={this.handleInputChange}
-                          value={this.state.newBarber.name}
-                      />
-                      <label htmlFor='new-barber-email'>Barber email</label>
-                          <input 
-                          type='text'
-                          name='email'
-                          id='new-barber-email'
-                          onChange={this.handleInputChange}
-                          value={this.state.newBarber.email}
-                          />
-      
-                      <label htmlFor='new-barber-image'>Barber Image</label>
-                          <input 
-                          type='text'
-                          name='image'
-                          id='new-barber-image'
-                          onChange={this.handleInputChange}
-                          value={this.state.newBarber.image}
-                          />
-      
-                      <label htmlFor='new-barber-experience'>Barber experience</label>
-                          <input
-                          type='text'
-                          name='experience'
-                          id='new-barber-experience'
-                          onChange={this.handleInputChange}
-                          value={this.state.newBarber.experience}
-                      />
-      
-                      <label htmlFor='new-barber-bio'>Barber Bio</label>
-                          <textarea
-                          type='text'
-                          name='bio'
-                          id='new-barber-bio'
-                          onChange={this.handleInputChange}
-                          value={this.state.newBarber.bio}
-                      />
-      
-                          <input type='submit' value='Update Barber'/>
-                      </form>
-          </div>
-          )
-    }
+        </Form>
+      </div>
+    );
+  }
 }

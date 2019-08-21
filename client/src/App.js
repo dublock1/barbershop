a3a948b7d666ddae470bee9e90c9bdf4961ee745
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-20 20:04:00 -0400

    switched Routes around

--- client/src/App.js
+++ client/src/App.js
@@ -22,10 +22,10 @@ function App() {
           <Route path='/barbershops/:barbershopId/barbers/new' component={BarberForm}/>
           <Route path='/barbershops/:barbershopId' component={Barbershop} />
           <Route exact path='/barbers' component={BarberList}/>
+          <Route path='/barbers/:barberId/styles/new' component={StyleForm}/>
           <Route exact path='/barbers/:barberId/edit' component={EditBarberForm}/>
           <Route path='/barbers/:barberId' component={Barber}/>
           <Route exact path='/styles' component={StyleList}/>
-          <Route path='/styles/new' component={StyleForm}/>
           <Route path='/styles/:styleId' component={Style}/>
         </Switch>
       </Router>

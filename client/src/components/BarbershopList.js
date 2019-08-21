1ef789558d2e5ed73070f05b942556eef6dbf48f
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-20 20:15:59 -0400

    added card bootstrap

--- client/src/components/BarbershopList.js
+++ client/src/components/BarbershopList.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import {Button} from 'react-bootstrap'
+import { Button } from "react-bootstrap";
 
 export default class BarbershopList extends Component {
   state = {
@@ -27,22 +27,37 @@ export default class BarbershopList extends Component {
   render() {
     const barbershopList = this.state.barbershops.map(barbershop => {
       return (
-        <div className="card bg-dark text-white" >
-          <Link key={barbershop._id} to={`/barbershops/${barbershop._id}`}>
-            <h2 className="card-title">Shop Name: {barbershop.name}</h2>
-            <h2 className="card-subtitle mb-2 text-muted">Address: {barbershop.address}</h2>
-            <h2>Current Barbers: {barbershop.currentBarber}</h2>
-            <img src={barbershop.image} className="card-img" />
-          </Link>
+        <div className="card mb-3" style={{ flexWrap: "wrap" }}>
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src={barbershop.image} className="card-img" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <Link
+                  key={barbershop._id}
+                  to={`/barbershops/${barbershop._id}`}
+                >
+                  <h2 className="card-title">Shop Name: {barbershop.name}</h2>
+                  <h2 className="card-text">Address: {barbershop.address}</h2>
+                  <p className="card-text">
+                    <small style={{ color: "black" }}>
+                      Click to view Barbers
+                    </small>
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       );
     });
     return (
-      <div> 
-        <Link to={'/barbershops/new'}>
-        <Button>Add Barbershop</Button>
+      <div>
+        <Link to={"/barbershops/new"}>
+          <Button>Add Barbershop</Button>
         </Link>
-        
+
         {barbershopList}
       </div>
     );

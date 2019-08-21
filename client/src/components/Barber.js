793e3ca5ec35e147ed7ea9b8f5241870a18ab295
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-08-20 20:08:54 -0400

    added bootstrap

--- client/src/components/Barber.js
+++ client/src/components/Barber.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {Redirect} from 'react-router-dom'
-import {Button} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import { Redirect } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class Barber extends Component {
   state = {
@@ -13,37 +13,63 @@ export default class Barber extends Component {
       experience: "",
       bio: "",
       image: ""
-    }
+    },
+    styles: []
   };
 
-  componentDidMount(){
-      axios.get(`/api/barbers/${this.props.match.params.barberId}`).then(res => {
-          this.setState({newBarber: res.data})
-      })
+  componentDidMount() {
+    axios.get(`/api/barbers/${this.props.match.params.barberId}`).then(res => {
+      this.setState({
+        newBarber: res.data.barber,
+        styles: res.data.styles
+      });
+    });
   }
 
   handleDelete = () => {
     axios.delete(`/api/barbers/${this.state.newBarber._id}`).then(() => {
-        this.setState({redirectToHome: true})
-    })
-}
+      this.setState({ redirectToHome: true });
+    });
+  };
   render() {
-    if(this.state.redirectToHome) {
-        return <Redirect to='/barbers' />
+    if (this.state.redirectToHome) {
+      return <Redirect to="/" />;
     }
     return (
-        <div>
-            <p>{this.state.newBarber.name}</p>
-            <p>{this.state.newBarber.email}</p>
-            <p>{this.state.newBarber.experience}</p>
-            <p>{this.state.newBarber.bio}</p>
-            <img src={this.state.newBarber.image} />
-            <Button onClick={this.handleDelete}>Delete Profile</Button>
-            <Link to={`/barbers/${this.state.newBarber._id}/edit`}>
-            <Button>Edit</Button>
-            </Link>
-            
-        </div>
-    )
+      <div>
+        <Link to='/'>
+          <Button>
+            Back to Shops
+          </Button>
+        </Link>
+        <img src={this.state.newBarber.image} />
+        <p> Barber name: {this.state.newBarber.name}</p>
+        <p>Email: {this.state.newBarber.email}</p>
+        <p> Experience: {this.state.newBarber.experience}</p>
+        <p> Bio: {this.state.newBarber.bio}</p>
+        <Button onClick={this.handleDelete}>Delete Profile</Button>
+        <Link to={`/barbers/${this.state.newBarber._id}/edit`}>
+          <Button>Edit</Button>
+        </Link>
+
+        {this.state.styles.map(style => {
+          return (
+            <div>
+              <Link className="link-color" to={`/styles/${style._id}`}>
+                {style.name}
+                
+              </Link>
+            </div>
+          );
+        })}
+        <Link
+          to={`/barbers/${
+            this.props.match.params.barberId
+          }/styles/new`}
+        >
+          <Button>Add Style</Button>
+        </Link>
+      </div>
+    );
   }
 }
--- client/src/components/BarberForm.js
+++ client/src/components/BarberForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import axios from 'axios'
-import {Redirect} from 'react-router-dom'
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { Form, Col, Button } from "react-bootstrap";
 
 export default class BarberForm extends Component {
   state = {
@@ -12,13 +13,13 @@ export default class BarberForm extends Component {
       image: "",
       barbershopId: this.props.match.params.barbershopId
     },
-    redirectToHome: false,
+    redirectToHome: false
   };
 
   handleSubmit = e => {
     e.preventDefault();
 
-    axios.post('/api/barbers', this.state.newBarber).then(() => {
+    axios.post("/api/barbers", this.state.newBarber).then(() => {
       this.setState({
         redirectToHome: true
       });
@@ -33,58 +34,77 @@ export default class BarberForm extends Component {
   };
   render() {
     if (this.state.redirectToHome) {
-      return <Redirect to={`/barbershops/${this.props.match.params.barbershopId}`} />;
+      return (
+        <Redirect to={`/barbershops/${this.props.match.params.barbershopId}`} />
+      );
     }
     return (
-    <div>
-        <form onSubmit={this.handleSubmit}>
-                <label htmlFor='new-barber-name'>Barber Name</label>
-                    <input
-                    type='text'
-                    name='name'
-                    id='new-barber-name'
-                    onChange={this.handleInputChange}
-                    value={this.state.newBarber.name}
-                />
-                <label htmlFor='new-barber-email'>Barber email</label>
-                    <input 
-                    type='text'
-                    name='email'
-                    id='new-barber-email'
-                    onChange={this.handleInputChange}
-                    value={this.state.newBarber.email}
-                    />
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridName">
+              <Form.Label htmlFor="new-barber-name">Barber Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                id="new-barber-name"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.name}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label htmlFor="new-barber-email">Barber email</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                id="new-barber-email"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.email}
+              />
+            </Form.Group>
+          </Form.Row>
 
-                <label htmlFor='new-barber-image'>Barber Image</label>
-                    <input 
-                    type='text'
-                    name='image'
-                    id='new-barber-image'
-                    onChange={this.handleInputChange}
-                    value={this.state.newBarber.image}
-                    />
-
-                <label htmlFor='new-barber-experience'>Barber experience</label>
-                    <input
-                    type='text'
-                    name='experience'
-                    id='new-barber-experience'
-                    onChange={this.handleInputChange}
-                    value={this.state.newBarber.experience}
-                />
-
-                <label htmlFor='new-barber-bio'>Barber Bio</label>
-                    <textarea
-                    type='text'
-                    name='bio'
-                    id='new-barber-bio'
-                    onChange={this.handleInputChange}
-                    value={this.state.newBarber.bio}
-                />
-
-                    <input type='submit' value='Add Barber'/>
-                </form>
-    </div>
-    )
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridImage">
+              <Form.Label htmlFor="new-barber-image">Barber Image</Form.Label>
+              <Form.Control
+                type="text"
+                name="image"
+                id="new-barber-image"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.image}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridExp">
+              <Form.Label htmlFor="new-barber-experience">
+                Barber experience
+              </Form.Label>
+              <Form.Control
+                type="text"
+                name="experience"
+                id="new-barber-experience"
+                onChange={this.handleInputChange}
+                value={this.state.newBarber.experience}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label htmlFor="new-barber-bio">Barber Bio</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="3"
+              type="text"
+              name="bio"
+              id="new-barber-bio"
+              onChange={this.handleInputChange}
+              value={this.state.newBarber.bio}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Add Barber
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
